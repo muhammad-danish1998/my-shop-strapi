5d46b3bec52f8bc961b38bc7e40eb78aa3eb2452
muhammad-danish1998 <iamdanish1998@gmail.com>
2022-08-12 01:02:28 +0500

    add stripe

--- pages/checkout.js
+++ pages/checkout.js
@@ -1,5 +1,6 @@
 import { UserIcon } from '@heroicons/react/outline'
 import { useEffect, useState } from 'react';
+import { loadStripe } from '@stripe/stripe-js';
 import { checkout } from "../checkout"
 const products = [
   {
@@ -14,7 +15,7 @@ const products = [
   },
   // More products...
 ]
-
+loadStripe(process.env.NEXT_PUBLIC_API_KEY);
 export default function Example({ cart }) {
 
 
@@ -27,6 +28,17 @@ export default function Example({ cart }) {
   });
 
   useEffect(() => {
+
+    const query = new URLSearchParams(window.location.search);
+    if (query.get('success')) {
+      console.log('Order placed! You will receive an email confirmation.');
+    }
+
+    if (query.get('canceled')) {
+      console.log('Order canceled -- continue to shop around and checkout when you’re ready.');
+    }
+
+
     let myTotal = 0
     for (let index = 0; index < cart.length; index++) {
       const element = cart[index];
@@ -210,18 +222,44 @@ export default function Example({ cart }) {
               >
                 Pay Now
               </button> */}
-              
+
+
+
             </form>
-            <button onClick={(() => {
-              checkout({
-                lineItems: [
-                  {
-                    price: "price_1LVYN2DTHFEX3w3fPxFppdDC",
-                    quantity: 1
-                  }
-                ]
-              })
-            })}>BUY!</button>
+            <form action="/api/checkout_sessions" method="POST">
+              <section>
+                <button type="submit" role="link">
+                  Pay Now
+                </button>
+              </section>
+              <style jsx>
+                {`
+          section {
+            background: #ffffff;
+            display: flex;
+            flex-direction: column;
+            width: 400px;
+            height: 112px;
+            border-radius: 6px;
+            justify-content: space-between;
+          }
+          button {
+            height: 36px;
+            background: #556cd6;
+            border-radius: 4px;
+            color: white;
+            border: 0;
+            font-weight: 600;
+            cursor: pointer;
+            transition: all 0.2s ease;
+            box-shadow: 0px 4px 5.5px 0px rgba(0, 0, 0, 0.07);
+          }
+          button:hover {
+            opacity: 0.8;
+          }
+        `}
+              </style>
+            </form>
 
           </div>
         </div>
